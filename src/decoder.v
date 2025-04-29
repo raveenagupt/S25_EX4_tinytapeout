f26171fe363e0b0c8b27215dc91715bd29946138
raveenagupt <93052623+raveenagupt@users.noreply.github.com>
2025-04-29 12:28:39 -0400

    Update decoder.v

--- src/decoder.v
+++ src/decoder.v
@@ -1,38 +1,161 @@
+module Decoder(
+    clk,
+    Row,
+    Col,
+    rst,
+    DecodeOut
+    );
 
-/*
-      -- 1 --
-     |       |
-     6       2
-     |       |
-      -- 7 --
-     |       |
-     5       3
-     |       |
-      -- 4 --
-*/
+// ==============================================================================================
+// 											Port Declarations
+// ==============================================================================================
+    input clk,rst;						// 100MHz onboard clock
+    input [3:0] Row;				// Rows on KYPD
+    output [3:0] Col;			// Columns on KYPD
+    output [3:0] DecodeOut;	// Output data
 
-module seg7 (
-    input wire [3:0] counter,
-    output reg [6:0] segments
-);
+// ==============================================================================================
+// 							  		Parameters, Regsiters, and Wires
+// ==============================================================================================
+	
+	// Output wires and registers
+	reg [3:0] Col;
+	reg [3:0] DecodeOut;
+	
+	// Count register
+	reg [19:0] sclk;
 
-    always @(*) begin
-        case(counter)
-            //                7654321
-            0:  segments = 7'b0111111;
-            1:  segments = 7'b0000110;
-            2:  segments = 7'b1011011;
-            3:  segments = 7'b1001111;
-            4:  segments = 7'b1100110;
-            5:  segments = 7'b1101101;
-            6:  segments = 7'b1111100;
-            7:  segments = 7'b0000111;
-            8:  segments = 7'b1111111;
-            9:  segments = 7'b1100111;
-            default:    
-                segments = 7'b0000000;
-        endcase
-    end
+// ==============================================================================================
+// 												Implementation
+// ==============================================================================================
+
+	always @(posedge clk) begin
+
+			// 1ms
+      if(rst) begin
+        DecodeOut <= 4'b0; 
+      end
+			if (sclk == 20'b00011000011010100000) begin
+				//C1
+				Col <= 4'b0111;
+				sclk <= sclk + 1'b1;
+			end
+			
+			// check row pins
+			else if(sclk == 20'b00011000011010101000) begin
+				//R1
+				if (Row == 4'b0111) begin
+					DecodeOut <= 4'b0001;		//1
+				end
+				//R2
+				else if(Row == 4'b1011) begin
+					DecodeOut <= 4'b0100; 		//4
+				end
+				//R3
+				else if(Row == 4'b1101) begin
+					DecodeOut <= 4'b0111; 		//7
+				end
+				//R4
+				else if(Row == 4'b1110) begin
+					DecodeOut <= 4'b0000; 		//0
+				end
+				sclk <= sclk + 1'b1;
+			end
+
+			// 2ms
+			else if(sclk == 20'b00110000110101000000) begin
+				//C2
+				Col<= 4'b1011;
+				sclk <= sclk + 1'b1;
+			end
+			
+			// check row pins
+			else if(sclk == 20'b00110000110101001000) begin
+				//R1
+				if (Row == 4'b0111) begin
+					DecodeOut <= 4'b0010; 		//2
+				end
+				//R2
+				else if(Row == 4'b1011) begin
+					DecodeOut <= 4'b0101; 		//5
+				end
+				//R3
+				else if(Row == 4'b1101) begin
+					DecodeOut <= 4'b1000; 		//8
+				end
+				//R4
+				else if(Row == 4'b1110) begin
+					DecodeOut <= 4'b1111; 		//F
+				end
+				sclk <= sclk + 1'b1;
+			end
+
+			//3ms
+			else if(sclk == 20'b01001001001111100000) begin
+				//C3
+				Col<= 4'b1101;
+				sclk <= sclk + 1'b1;
+			end
+			
+			// check row pins
+			else if(sclk == 20'b01001001001111101000) begin
+				//R1
+				if(Row == 4'b0111) begin
+					DecodeOut <= 4'b0011; 		//3	
+				end
+				//R2
+				else if(Row == 4'b1011) begin
+					DecodeOut <= 4'b0110; 		//6
+				end
+				//R3
+				else if(Row == 4'b1101) begin
+					DecodeOut <= 4'b1001; 		//9
+				end
+				//R4
+				else if(Row == 4'b1110) begin
+					DecodeOut <= 4'b1110; 		//E
+				end
+
+				sclk <= sclk + 1'b1;
+			end
+
+			//4ms
+			else if(sclk == 20'b01100001101010000000) begin
+				//C4
+				Col<= 4'b1110;
+				sclk <= sclk + 1'b1;
+			end
+
+			// Check row pins
+			else if(sclk == 20'b01100001101010001000) begin
+				//R1
+				if(Row == 4'b0111) begin
+					DecodeOut <= 4'b1010; //A
+				end
+				//R2
+				else if(Row == 4'b1011) begin
+					DecodeOut <= 4'b1011; //B
+				end
+				//R3
+				else if(Row == 4'b1101) begin
+					DecodeOut <= 4'b1100; //C
+				end
+				//R4
+				else if(Row == 4'b1110) begin
+					DecodeOut <= 4'b1101; //D
+				end
+				sclk <= 20'b00000000000000000000;
+			end
+
+			// Otherwise increment
+			else begin
+				sclk <= sclk + 1'b1;
+			end
+			
+	end
 
 endmodule
+
+
+
 
